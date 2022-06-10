df70cb2ea714599d0398c90893d365f9460583a6
Coltisor Victor <v.coltisor@rankingcoach.com>
2022-06-11 00:39:16 +0300

    Correct import

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {Map} from "./src/screens/map";
+import { Map } from "./src/screens/Map/Map";
 
 export default function App() {
   return (
