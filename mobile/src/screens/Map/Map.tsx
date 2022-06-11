8366df8fde456ff6039834cdcc615e11aba55065
Coltisor Victor <v.coltisor@rankingcoach.com>
2022-06-11 12:37:39 +0300

    Update Map.tsx

--- mobile/src/screens/Map/Map.tsx
+++ mobile/src/screens/Map/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from "react-native";
-import {MapScreenProps} from "../RouteProps";
+import { MapScreenProps } from "../RouteProps";
 
 export const Map = (props: MapScreenProps) => {
   console.log('MAP');
