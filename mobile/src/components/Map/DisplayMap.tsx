98804935832c57903f0d07d5e1ac406e975f86f7
DociPetru <docipetru03@gmail.com>
2022-06-11 12:23:49 +0300

    create display map

--- mobile/src/components/Map/DisplayMap.tsx
+++ mobile/src/components/Map/DisplayMap.tsx
@@ -1,20 +1,33 @@
 import React, { useRef, useState } from 'react';
 import H from "@here/maps-api-for-javascript";
 import { useEffect } from 'react';
-import { StyleSheet, View, ViewComponent } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 export const DisplayMap = () => {
     const apiKey = "VydPmif68dXoHyntRnVzyFQh9ND9D1C7mCrtHEnyKH4"
+    const [currentLocation , setCurrentLocation] = useState<any>(null)
 
-   const mapRef = useRef<HTMLElement>();
+    const mapRef = useRef<HTMLElement>();
 
-   const [state , setState] = useState<any>(null)
+    const findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
+9
+        console.log(location);  
+
+        setCurrentLocation(location)
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+   findCoordinates();
 
    useEffect(()=>{
-
     if (mapRef.current) {
-    const platform = new H.service.Platform({
-        apikey: 'VydPmif68dXoHyntRnVzyFQh9ND9D1C7mCrtHEnyKH4'
+      const platform = new H.service.Platform({
+        apikey: apiKey
       });
 
       const layers = platform.createDefaultLayers();
@@ -27,23 +40,16 @@ export const DisplayMap = () => {
           zoom: 4,
         },
       );
-
-
       const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(mapTMP));
-
       const ui = H.ui.UI.createDefault(mapTMP, layers);
 
-      setState(mapTMP)
-        
+      addMarkersToMap(mapTMP)  
     }
    } , []);
-  
 
     return (
         <View ref={mapRef} style={styles.container}></View>
-
-    )
-  
+    )  
 }
 
 const styles = StyleSheet.create({
@@ -51,3 +57,15 @@ const styles = StyleSheet.create({
         width: '100vw', height:'100vh', position: 'relative'
     }
   });
+
+
+
+const addMarkersToMap = (map : any) => {
+    var parisMarker = new H.map.Marker({lat:48.8567, lng:2.3508});
+    map.addObject(parisMarker);
+
+    var romeMarker = new H.map.Marker({lat:41.9, lng: 12.5});
+    map.addObject(romeMarker);
+
+
+}
--- mobile/src/screens/Map/Map.tsx
+++ mobile/src/screens/Map/Map.tsx
@@ -1,24 +1,11 @@
 import React, { useState } from 'react'
 import { DisplayMap } from '../../components/Map/DisplayMap';
 import { Alert } from "react-native"
+import GetLocation from 'react-native-get-location'
 
 export const Map = () => {
-  const [currentLocation , setCurrentLocation] = useState<any>(null)
 
- const findCoordinates = () => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const location = JSON.stringify(position);
-
-        setCurrentLocation(location)
-      },
-      error => Alert.alert(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  };
-
-  console.log(currentLocation);
-  
+ 
 
   return (
     <DisplayMap/>
