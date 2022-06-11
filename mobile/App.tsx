79ee9efe60ff3b3da1424293221c54344652eb94
Coltisor Victor <v.coltisor@rankingcoach.com>
2022-06-11 14:38:50 +0300

    repair

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,8 +6,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from "./src/screens/Home/Home";
 import {Map} from "./src/screens/Map/Map";
 import {Rides} from "./src/screens/Rides/Rides";
-import WalletConnectProvider from '@walletconnect/react-native-dapp';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Stack = createNativeStackNavigator();
@@ -15,11 +13,6 @@ const Stack = createNativeStackNavigator();
 export default function App() {
 
   return (
-    <WalletConnectProvider
-      redirectUrl={Platform.OS === 'web' ? window.location.origin : 'yourappscheme://'}
-      storageOptions= {{
-        asyncStorage: AsyncStorage,
-      }}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Login" component={Login} />
@@ -28,7 +21,6 @@ export default function App() {
           <Stack.Screen name="Rides" component={Rides} />
         </Stack.Navigator>
       </NavigationContainer>
-    </WalletConnectProvider>
   );
 }
 
--- mobile/src/screens/Login/Login.tsx
+++ mobile/src/screens/Login/Login.tsx
@@ -2,21 +2,17 @@ import React from 'react';
 import { Button } from "../../components/Button/Button";
 import { View } from "react-native";
 import { LoginScreenProps } from '../RouteProps';
-import { WalletConnectProvider } from '@elrondnetwork/erdjs-wallet-connect-provider/out';
-import { useWalletConnect } from '@walletconnect/react-native-dapp';
 
 
-const bridgeUrl = "https://bridge.walletconnect.org";
 
 
 export const Login = (props: LoginScreenProps) => {
   const { navigation } = props;
-  const connector = useWalletConnect();
 
 
   return (
     <View>
-      <Button onPress={() => connector.connect()}>Connect</Button>
+      <Button onPress={() => navigation.navigate('Home')}>Connect</Button>
     </View>
   )
 }
