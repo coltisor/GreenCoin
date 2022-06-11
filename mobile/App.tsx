8271f9961b0ce5131bc033be5957640a1f92d6ba
Coltisor Victor <v.coltisor@rankingcoach.com>
2022-06-11 13:41:20 +0300

    Add register component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,23 +6,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from "./src/screens/Home/Home";
 import {Map} from "./src/screens/Map/Map";
 import {Rides} from "./src/screens/Rides/Rides";
+import { registerRootComponent } from 'expo';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
- 
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home}  options={{headerLeft: null}}/>
-        <Stack.Screen name="Map" component={Map} />
-        <Stack.Screen name="Rides" component={Rides}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} options={{ headerLeft: null }} />
+          <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="Rides" component={Rides} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
+
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
